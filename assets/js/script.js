55691a24efd2d532fb2dc3004842eae6d04a5171
Rodriguez Nicolas <nicoxygen@gmail.com>
2022-06-03 19:47:09 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-    const cookieBox = document.querySelector(".wrapper"),
+    const cookieBox = document.querySelector(".cookie_wrapper"),
     acceptBtn = cookieBox.querySelector("button");
 
     acceptBtn.onclick = ()=>{
       //setting cookie for 1 month, after one month it'll be expired automatically
-      document.cookie = "CookieBy=CodingNepal; max-age="+60*60*24*30;
+      document.cookie = "CookieBy=Nicoxygen; max-age="+60*60*24*30;
       if(document.cookie){ //if cookie is set
         cookieBox.classList.add("hide"); //hide cookie box
       }else{ //if cookie not set then alert an error
-        alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+        alert("Le cookie ne peut pas être défini ! Veuillez débloquer ce site de la configuration des cookies de votre navigateur.");
       }
     }
-    let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
+    let checkCookie = document.cookie.indexOf("CookieBy=Nicoxygen"); //checking our cookie
     //if cookie is set then hide the cookie box else show it
     checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
